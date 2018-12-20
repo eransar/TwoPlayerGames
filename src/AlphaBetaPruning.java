508c0e07773cb487956c65d6ce7d222253582bf8
idanf <idanforu@gmail.com>
2018-12-20 14:50:23 +0200

    fix work

--- src/AlphaBetaPruning.java
+++ src/AlphaBetaPruning.java
@@ -18,34 +18,28 @@ public class AlphaBetaPruning implements ISolver
 	
 	private double AlphaBetaPruningAlgorithm(Node 	node, double 	p_alpha, double 	p_beta)
 	{
-		double value;
-		if (node.getNodeType() == Node.NodeType.MAX)
-			value = -Double.MAX_VALUE;
-		else
-			value = Double.MAX_VALUE;
-
-		// Explore Child Nodes
-		List<Node> children = node.getNodeChildren();
-		for (Node child : children) {
-			double childValue;
-			if (child.isTerminalNode())
-				childValue = child.getScore();
-			else
-				childValue = AlphaBetaPruningAlgorithm(child,p_alpha,p_beta);
-			if(!(p_beta<=p_alpha)) {
-				if (node.getNodeType() == Node.NodeType.MAX) {
-					if (childValue <= p_alpha)
-						return p_alpha;
-					value = Double.max(value, childValue);
-				} else {
-					if (childValue >= p_beta)
-						return p_beta;
-					value = Double.min(value, childValue);
-				}
+		if(node.isTerminalNode())
+			return node.getScore();
+		if(node.getNodeType() == Node.NodeType.MAX){
+			double value = - Double.MAX_VALUE;
+			for(Node n : node.getNodeChildren()){
+				value = Double.max(value,AlphaBetaPruningAlgorithm(n,p_alpha,p_beta));
+				p_alpha = Double.max(p_alpha,value);
+				if(p_alpha>=p_beta)
+					break;
 			}
+			return value;
 		}
-
-		return value;
+		else{
+			double value = Double.MAX_VALUE;
+			for(Node n : node.getNodeChildren()){
+				value = Double.min(value,AlphaBetaPruningAlgorithm(n,p_alpha,p_beta));
+				p_beta = Double.min(p_beta,value);
+				if(p_alpha>=p_beta)
+					break;
+			}
+			return value;
+		}
 	}
 	
 }
